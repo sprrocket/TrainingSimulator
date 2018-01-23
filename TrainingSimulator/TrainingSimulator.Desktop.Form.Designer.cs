5dda2cf0bddd8403a3c741ee9083ecdbabb167e2
Caleb Wellauer <caleb.wellauer@gmail.com>
2018-01-23 16:02:38 -0700

    lots

---
+++ TrainingSimulator/TrainingSimulator.Desktop.Form.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace TrainingSimulator
+{
+    partial class TrainingSimulatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.mapGrid0 = new System.Windows.Forms.Button();
+            this.mapGrid1 = new System.Windows.Forms.Button();
+            this.mapGrid2 = new System.Windows.Forms.Button();
+            this.mapGrid3 = new System.Windows.Forms.Button();
+            this.mapGrid4 = new System.Windows.Forms.Button();
+            this.mapGrid5 = new System.Windows.Forms.Button();
+            this.mapGrid6 = new System.Windows.Forms.Button();
+            this.mapGrid7 = new System.Windows.Forms.Button();
+            this.mapGrid8 = new System.Windows.Forms.Button();
+            this.mapGrid9 = new System.Windows.Forms.Button();
+            this.mapGrid10 = new System.Windows.Forms.Button();
+            this.mapGrid11 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.mapPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mapPanel
+            // 
+            this.mapPanel.ColumnCount = 3;
+            this.mapPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33444F));
+            this.mapPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33445F));
+            this.mapPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33111F));
+            this.mapPanel.Controls.Add(this.mapGrid0, 0, 0);
+            this.mapPanel.Controls.Add(this.mapGrid1, 1, 0);
+            this.mapPanel.Controls.Add(this.mapGrid2, 2, 0);
+            this.mapPanel.Controls.Add(this.mapGrid3, 0, 1);
+            this.mapPanel.Controls.Add(this.mapGrid4, 1, 1);
+            this.mapPanel.Controls.Add(this.mapGrid5, 2, 1);
+            this.mapPanel.Controls.Add(this.mapGrid6, 0, 2);
+            this.mapPanel.Controls.Add(this.mapGrid7, 1, 2);
+            this.mapPanel.Controls.Add(this.mapGrid8, 2, 2);
+            this.mapPanel.Controls.Add(this.mapGrid9, 0, 3);
+            this.mapPanel.Controls.Add(this.mapGrid10, 1, 3);
+            this.mapPanel.Controls.Add(this.mapGrid11, 2, 3);
+            this.mapPanel.Location = new System.Drawing.Point(13, 13);
+            this.mapPanel.Name = "mapPanel";
+            this.mapPanel.RowCount = 4;
+            this.mapPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.mapPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.mapPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.mapPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.mapPanel.Size = new System.Drawing.Size(428, 539);
+            this.mapPanel.TabIndex = 0;
+            this.mapPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mapPanel_Paint);
+            // 
+            // mapGrid0
+            // 
+            this.mapGrid0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid0.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid0.Location = new System.Drawing.Point(3, 3);
+            this.mapGrid0.Name = "mapGrid0";
+            this.mapGrid0.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid0.TabIndex = 0;
+            this.mapGrid0.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid1
+            // 
+            this.mapGrid1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid1.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid1.Location = new System.Drawing.Point(145, 3);
+            this.mapGrid1.Name = "mapGrid1";
+            this.mapGrid1.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid1.TabIndex = 1;
+            this.mapGrid1.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid2
+            // 
+            this.mapGrid2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid2.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid2.Location = new System.Drawing.Point(287, 3);
+            this.mapGrid2.Name = "mapGrid2";
+            this.mapGrid2.Size = new System.Drawing.Size(138, 128);
+            this.mapGrid2.TabIndex = 2;
+            this.mapGrid2.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid3
+            // 
+            this.mapGrid3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid3.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid3.Location = new System.Drawing.Point(3, 137);
+            this.mapGrid3.Name = "mapGrid3";
+            this.mapGrid3.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid3.TabIndex = 3;
+            this.mapGrid3.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid4
+            // 
+            this.mapGrid4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid4.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid4.Location = new System.Drawing.Point(145, 137);
+            this.mapGrid4.Name = "mapGrid4";
+            this.mapGrid4.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid4.TabIndex = 4;
+            this.mapGrid4.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid5
+            // 
+            this.mapGrid5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid5.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid5.Location = new System.Drawing.Point(287, 137);
+            this.mapGrid5.Name = "mapGrid5";
+            this.mapGrid5.Size = new System.Drawing.Size(138, 128);
+            this.mapGrid5.TabIndex = 5;
+            this.mapGrid5.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid6
+            // 
+            this.mapGrid6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid6.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid6.Location = new System.Drawing.Point(3, 271);
+            this.mapGrid6.Name = "mapGrid6";
+            this.mapGrid6.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid6.TabIndex = 6;
+            this.mapGrid6.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid7
+            // 
+            this.mapGrid7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid7.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid7.Location = new System.Drawing.Point(145, 271);
+            this.mapGrid7.Name = "mapGrid7";
+            this.mapGrid7.Size = new System.Drawing.Size(136, 128);
+            this.mapGrid7.TabIndex = 7;
+            this.mapGrid7.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid8
+            // 
+            this.mapGrid8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid8.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid8.Location = new System.Drawing.Point(287, 271);
+            this.mapGrid8.Name = "mapGrid8";
+            this.mapGrid8.Size = new System.Drawing.Size(138, 128);
+            this.mapGrid8.TabIndex = 8;
+            this.mapGrid8.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid9
+            // 
+            this.mapGrid9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid9.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid9.Location = new System.Drawing.Point(3, 405);
+            this.mapGrid9.Name = "mapGrid9";
+            this.mapGrid9.Size = new System.Drawing.Size(136, 131);
+            this.mapGrid9.TabIndex = 9;
+            this.mapGrid9.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid10
+            // 
+            this.mapGrid10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid10.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid10.Location = new System.Drawing.Point(145, 405);
+            this.mapGrid10.Name = "mapGrid10";
+            this.mapGrid10.Size = new System.Drawing.Size(136, 131);
+            this.mapGrid10.TabIndex = 10;
+            this.mapGrid10.UseVisualStyleBackColor = true;
+            // 
+            // mapGrid11
+            // 
+            this.mapGrid11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.mapGrid11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapGrid11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mapGrid11.ForeColor = System.Drawing.Color.Transparent;
+            this.mapGrid11.Location = new System.Drawing.Point(287, 405);
+            this.mapGrid11.Name = "mapGrid11";
+            this.mapGrid11.Size = new System.Drawing.Size(138, 131);
+            this.mapGrid11.TabIndex = 11;
+            this.mapGrid11.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(494, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(236, 165);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // TrainingSimulatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 642);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.mapPanel);
+            this.Name = "TrainingSimulatorForm";
+            this.Text = "Form1";
+            this.mapPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mapPanel;
+        private System.Windows.Forms.Button mapGrid0;
+        private System.Windows.Forms.Button mapGrid1;
+        private System.Windows.Forms.Button mapGrid2;
+        private System.Windows.Forms.Button mapGrid3;
+        private System.Windows.Forms.Button mapGrid4;
+        private System.Windows.Forms.Button mapGrid5;
+        private System.Windows.Forms.Button mapGrid6;
+        private System.Windows.Forms.Button mapGrid7;
+        private System.Windows.Forms.Button mapGrid8;
+        private System.Windows.Forms.Button mapGrid9;
+        private System.Windows.Forms.Button mapGrid10;
+        private System.Windows.Forms.Button mapGrid11;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TrainingSimulator/TrainingSimulator.Desktop.Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrainingSimulator.Data;
+
+namespace TrainingSimulator
+{
+    public partial class TrainingSimulatorForm : Form
+    {
+        public TrainingSimulatorForm()
+        {
+            InitializeComponent();
+            TrainingSimulator.Data.Map map = new Map();
+            map.LoadMapData(TrainingSimulator.Data.RoomData.data);
+            Room currentRoom = map.roomLayout[3, 1];
+            richTextBox1.Text = map.roomLayout[3, 1].GetDescription;
+            mapGrid10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            
+            mapGrid10.BackgroundImage = global::TrainingSimulator.Properties.Resources.star1;
+            mapGrid10.BackgroundImage = null;
+            
+        }
+
+        private void mapPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
